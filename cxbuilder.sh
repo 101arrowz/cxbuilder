342fcd6c17a9239dbe8ec1557fd9574cee1786ec
101arrowz <arjunbarrett@gmail.com>
2024-05-04 21:25:39 -0400

    even more progress

--- cxbuilder.sh
+++ cxbuilder.sh
@@ -11,6 +11,7 @@ CXB_TEMP="${CXB_TEMP:-.cxbuilder}"
 verbose=0
 is_macos=0; test "`uname -s`" = "Darwin" && is_macos=1
 is_interactive=0; test -t 0 && is_interactive=1
+wine_dir=
 use_gptk=
 use_dxvk=
 gptk_dir=
@@ -24,6 +25,11 @@ err() {
     out="${out%?}"
     eprint "$out"
     log_write "$out"
+}
+
+exite() {
+    err "$@"
+    exit 1
 }
 
 warn() {
@@ -71,44 +77,51 @@ eprintn() { printf "$1\n" >&2; }
 log_write() { printf "$1" >> $CXB_LOG_FILE; }
 
 usage() {
-    eprintn "$0 [-v] [-x] [--gptk DIR] [--dxvk DIR] [-o DEST_DIR] [SOURCE_DIR]"
+    eprintn "$0 [-v] [-x] [--wine dir] [--gptk dir] [--dxvk dir] [--out dest_dir] [source_dir]"
     eprintn ""
     eprintn "Arguments:"
     eprintn "    -h, --help           Prints this help message and exits"
     eprintn "    -v, --verbose        Enables verbose logging of build commands and results"
     eprintn "    -x, --no-prompt      Run non-interactively"
     eprintn ""
+    eprintn "    -w, --wine           Uses the Wine sources from the specified directory."
+    eprintn "                         CXBuilder is designed specifically to work with the"
+    eprintn "                         Wine sources provided with the open-source distribution"
+    eprintn "                         of CrossOver, which include various \"hacks\" to make"
+    eprintn "                         software that breaks with mainline Wine work anyway."
+    eprintn "                         However, CXBuilder should work the original Wine too,"
+    eprintn "                         or any fork. Defaults to [source_dir]/wine."
+    eprintn ""
     eprintn "    --no-gptk            Disable use of Direct3D DLLs from Apple's Game Porting"
     eprintn "                         Toolkit (GPTk). GPTk is supported only on Apple Silicon"
     eprintn "                         Macs. GPTk disabled by default on all other devices."
     eprintn ""
-    eprintn "    --gptk DIR           Uses the Game Porting Toolkit from the specified"
+    eprintn "    --gptk dir           Uses the Game Porting Toolkit from the specified"
     eprintn "                         directory. This should have a redist/ subdirectory with"
     eprintn "                         Apple's proprietary Direct3D -> Metal translation"
-    eprintn "                         layer libraries inside. Defaults to SOURCE_DIR/gptk."
+    eprintn "                         layer libraries inside. Defaults to [source_dir]/gptk."
     eprintn ""
     eprintn "    --no-dxvk            Disables the use of Direct3D DLLs from DXVK. Note that"
     eprintn "                         if GPTk is enabled, DXVK will only be used for 32-bit"
     eprintn "                         software (as GPTk does not support 32-bit Direct3D)."
     eprintn ""
-    eprintn "    --dxvk DIR           Uses the DXVK build from the specified directory. This"
+    eprintn "    --dxvk dir           Uses the DXVK build from the specified directory. This"
     eprintn "                         should have x32/ and x64/ subdirectories with Direct3D"
     eprintn "                         DLLs. Note that if both GPTk and DXVK are enabled, GPTk"
     eprintn "                         will take priority (i.e. only the x32/ DLLs from DXVK"
-    eprintn "                         will be used). Defaults to SOURCE_DIR/dxvk."
+    eprintn "                         will be used). Defaults to [source_dir]/dxvk."
     eprintn ""
-    eprintn "    -o, --out DEST_DIR   Where to generate the final Wine build. This directory"
+    eprintn "    -o, --out dest_dir   Where to generate the final Wine build. This directory"
     eprintn "                         will contain a similar structure to /usr/local (and"
     eprintn "                         that is indeed a valid value for this argument) after"
     eprintn "                         the build completes. However, the build can be placed"
     eprintn "                         anywhere on your system and may be moved around freely"
     eprintn "                         afterwards, so long as relative symlinks are preserved."
-    eprintn "                         Defaults to SOURCE_DIR/build."
+    eprintn "                         Defaults to [source_dir]/build."
     eprintn ""
-    eprintn "    SOURCE_DIR           The directory in which the source files are present."
+    eprintn "    source_dir           The directory in which the source files are present."
     eprintn "                         It is not necessary to use this directory, but it can"
     eprintn "                         be used to create a build with only one argument."
-    eprintn ""
 }
 
 while test $# != 0
@@ -117,6 +130,7 @@ do
     -h|--help) usage; exit 0;;
     -v|--verbose) verbose=1;;
     -x|--no-prompt) is_interactive=0;;
+    -w|--wine) shift; wine_dir="$1";;
     --no-gptk) use_gptk=0;;
     --no-dxvk) use_dxvk=0;;
     --gptk) shift; use_gptk=1; gptk_dir="$1";;
@@ -197,35 +211,55 @@ use_dxvk="${use_dxvk:-1}"
 
 if test -z "$source_dir"; then
     if test -z "$dst_dir"; then
-        err "No destination directory specified; specify a source directory or use -o your/build/directory"
-        exit 1
+        exite "No destination directory specified; specify a source directory or use -o your/build/directory"
+    fi
+
+    if test -z "$wine_dir"; then
+        exite "No Wine directory specified; specify a source directory or use -w your/wine/sources/directory"
     fi
 
     if test $use_gptk = 1 && test -z "$gptk_dir"; then
-        err "GPTk is enabled but no GPTk directory was provided; specify a source directory, use --no-gptk, or use --gptk your/gptk/directory"
-        exit 1
+        exite "GPTk is enabled but no GPTk directory was provided; specify a source directory, use --no-gptk, or use --gptk your/gptk/directory"
     fi
 
     if test $use_dxvk = 1 && test -z "$dxvk_dir"; then
-        err "DXVK is enabled but no DXVK directory was provided; specify a source directory, use --no-dxvk, or use --dxvk your/dxvk/directory"
-        exit 1
+        exite "DXVK is enabled but no DXVK directory was provided; specify a source directory, use --no-dxvk, or use --dxvk your/dxvk/directory"
     fi
 else
     source_dir_err="cannot use source directory $source_dir"
 
     if ! test -e "$source_dir"; then
-        err "%s; does not exist" "$source_dir_err"
-        exit 1
+        exite "%s; does not exist" "$source_dir_err"
     fi
 
     if ! test -d "$source_dir"; then
-        err "%s; not a directory" "$source_dir_err"
-        exit 1
+        exite "%s; not a directory" "$source_dir_err"
     fi
 fi
 
+dst_dir="${dst_dir:-$source_dir/build}"
+scratch_dir="$dst_dir/$CXB_TEMP"
+wine_dir="${wine_dir:-$source_dir/wine}"
 dxvk_dir="${dxvk_dir:-$source_dir/dxvk}"
 gptk_dir="${gptk_dir:-$source_dir/gptk}"
+
+info "using wine from $wine_dir"
+
+wine_err="cannot load wine from $wine_dir"
+
+if test ! -e "$wine_dir"; then
+    exite "%s; does not exist" "$wine_err"
+fi
+
+if test ! -d "$wine_dir"; then
+    exite "%s; not a directory" "$wine_err"
+fi
+
+if test ! -f "$wine_dir/configure"; then
+    exite "%s; could not locate configure script"
+fi
+
+info "$wine_dir has valid Wine sources"
 
 if test $use_gptk = 1; then
     info "using gptk from $gptk_dir"
@@ -233,13 +267,11 @@ if test $use_gptk = 1; then
     gptk_err="cannot load GPTk from $gptk_dir"
 
     if test ! -e "$gptk_dir"; then
-        err "%s; does not exist" "$gptk_err"
-        exit 1
+        exite "%s; does not exist" "$gptk_err"
     fi
 
     if test ! -d "$gptk_dir"; then
-        err "%s; not a directory" "$gptk_err"
-        exit 1
+        exite "%s; not a directory" "$gptk_err"
     fi
 
     gptk_paths="/redist/lib/external/D3DMetal.framework /redist/lib/external/libd3dshared.dylib \
@@ -265,13 +297,11 @@ if test $use_dxvk = 1; then
     dxvk_err="cannot load DXVK from $dxvk_dir"
 
     if test ! -e "$dxvk_dir"; then
-        err "%s; does not exist" "$dxvk_err"
-        exit 1
+        exite "%s; does not exist" "$dxvk_err"
     fi
 
     if test ! -d "$dxvk_dir"; then
-        err "%s; not a directory" "$dxvk_err"
-        exit 1
+        exite "%s; not a directory" "$dxvk_err"
     fi
 
     dxvk_paths="/x32/d3d9.dll /x32/d3d10core.dll /x32/d3d11.dll /x32/dxgi.dll \
@@ -290,8 +320,7 @@ if test $use_dxvk = 1; then
     if test -n "$missing_dxvk_paths"; then
         warn "missing the following DXVK DLLs: $missing_dxvk_paths"
         if test $any_dxvk_path = 0; then
-            err "%s; no DXVK DLLs found" "$dxvk_err"
-            exit 1
+            exite "%s; no DXVK DLLs found" "$dxvk_err"
         fi
         prompt_continue "The provided DVXK build seems incomplete. Continue?"
     fi
@@ -305,57 +334,48 @@ if ! test -d "$dst_dir"; then
     info "creating build directory $dst_dir"
 
     if test -e "$dst_dir"; then
-        err "failed to create $dst_dir; path exists"
-        exit 1
+        exite "failed to create $dst_dir; path exists"
     fi
 
     if mkdir "$dst_dir"; then
         info "successfully created $dst_dir"
     else
-        err "failed to create $dst_dir"
-        exit 1
+        exite "failed to create $dst_dir"
     fi
 else
-    if test -d "$dst_dir/$CXB_TEMP"; then
-        info "found prior CXBuilder run in $dst_dir"
+    if test -d "$scratch_dir"; then
+        info "found prior CXBuilder tempdir in $dst_dir"
     fi
 fi
 
+if ! test -d "$scratch_dir"; then
+    mkdir "$scratch_dir" || if tmp_dir="`mktemp -d 2> /dev/null`"; then
+        scratch_dir="$tmp_dir"
+    else
+        exite "failed to create CXBuilder tempdir $scratch_dir"
+    fi
+    info "successfully created $scratch_dir"
+
+fi
+
+info "validation complete; starting build"
+
+mvk_dir="$scratch_dir/molten-vk"
+wine_build_dir="$scratch_dir/wine-build"
+
+# TODO: install MoltenVK, gettext, etc.
+if test $is_macos = 1; then
+    
+end
+
+wineconf_args="--disable-option-checking --disable-tests --enable-archs=i386,x86_64 --without-alsa \
+--without-capi --with-coreaudio --with-cups --without-dbus --without-fontconfig --with-freetype \
+--with-gettext --without-gettextpo --without-gphoto --with-gnutls --without-gssapi --with-gstreamer \
+--with-inotify --without-krb5 --with-mingw  --without-netapi --with-opencl --with-opengl --without-oss \
+--with-pcap --with-pcsclite --with-pthread --without-pulse --without-sane --with-sdl --without-udev \
+--with-unwind --without-usb --without-v4l2 --with-vulkan --without-wayland --without-x $CXB_CONF_ARGS"
+
+
+"$wine_dir"/configure $wineconf_args --prefix="$wine_build_dir"
+
 # TODO: which file?
-# configure
-#     --disable-option-checking \
-#     --disable-tests \         
-#     --enable-archs=i386,x86_64 \
-#     --without-alsa \
-#     --without-capi \
-#     --with-coreaudio \
-#     --with-cups \
-#     --without-dbus \
-#     --without-fontconfig \
-#     --with-freetype \
-#     --with-gettext \
-#     --without-gettextpo \
-#     --without-gphoto \
-#     --with-gnutls \
-#     --without-gssapi \
-#     --with-gstreamer \
-#     --with-inotify \
-#     --without-krb5 \
-#     --with-mingw \
-#     --without-netapi \
-#     --with-opencl \
-#     --with-opengl \
-#     --without-oss \
-#     --with-pcap \
-#     --with-pcsclite \
-#     --with-pthread \
-#     --without-pulse \
-#     --without-sane \
-#     --with-sdl \
-#     --without-udev \
-#     --with-unwind \
-#     --without-usb \
-#     --without-v4l2 \
-#     --with-vulkan \
-#     --without-wayland \
-#     --without-x
